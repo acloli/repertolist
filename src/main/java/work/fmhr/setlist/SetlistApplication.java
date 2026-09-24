4c953ab5f5795ad5ede6b17eb090cdd27b947fab
fumihiro_to <fumihiro.to@fmhr.work>
2026-09-24 12:27:50 +0900

    chore: プロジェクトの初期構成を作成

---
+++ src/main/java/work/fmhr/setlist/SetlistApplication.java
@@ -0,0 +1,13 @@
+package work.fmhr.setlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SetlistApplication.class, args);
+	}
+
+}
