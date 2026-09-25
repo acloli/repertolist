27f3841dad44000ef66f5605f60c39f77af16694
fumihiro_to <fumihiro.to@fmhr.work>
2026-09-25 15:13:21 +0900

    fix: ホーム画面と一覧画面にdynamicを追加

--- frontend/src/app/songs/page.tsx
+++ frontend/src/app/songs/page.tsx
@@ -1,6 +1,8 @@
 import { fetchSongs } from "@/lib/api";
 import SongListTable from "./SongListTable";
 
+export const dynamic = "force-dynamic";
+
 export default async function SongsPage() {
   const songs = await fetchSongs();
 
